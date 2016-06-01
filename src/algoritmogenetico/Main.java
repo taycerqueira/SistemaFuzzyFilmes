f41c185086c9aa12c0deb0264c4f5dfd83cfe226
Tay Cerqueira <engcomp.tayane@gmail.com>
2016-06-01 00:13:33 -0300

    casa

--- src/algoritmogenetico/Main.java
+++ src/algoritmogenetico/Main.java
@@ -1,10 +1,40 @@
 package algoritmogenetico;
 
-public class Main {
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class Main {
+	
+	static String nomeBase = "basefilmes_53atributos";
+	static DataSource source;
+
+	public static void main(String[] args) throws Exception {
+		
+		source = new DataSource (nomeBase + ".arff");
+		
+		//Cria o sistema fuzzy a partir do arquivo fcl
+		FIS fis = FIS.load((nomeBase+".fcl"), true);
+
+		if (fis == null) {
+			System.err.println("Can't load file: '" + nomeBase + "'");
+			System.exit(1);
+		}
+		
+		ArrayList<Rule> regras = (ArrayList<Rule>) fis.getFunctionBlock(null).getFuzzyRuleBlock("No1").getRules();
+		int quantidadeRegras = regras.size();
+		
+		Rule[] vetorRegras = new Rule[quantidadeRegras];
+		
 
 	}
+	
+	public static void calculaFitness(){
+		
+	}
+
+	
 
 }
